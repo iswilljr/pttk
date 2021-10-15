c1ba285f877eaa50efd676e5fdd72ca2a4ed40a7
WillJrJU <91134158+WillJrJU@users.noreply.github.com>
2021-10-14 21:11:29 -0500

    Sprint 3 - Equipo 10

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, redirect, request
 from flask.helpers import flash
 import os
+
 app = Flask(__name__)
 app.secret_key=os.urandom(24)
-
 logged=False
 users=['123']
 passwords=['123']
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def Index():
+    global logged
+    logged=False
     return render_template('index.html')
 
 @app.route('/Login', methods=['GET','POST'])
@@ -18,20 +20,25 @@ def Login():
     if request.method == 'POST':
         user=request.form['user']
         passw=request.form['pass']
-        if user=='123' and passw=='123':
-            logged=True
-            return redirect('/Inicio')
-        else:
-            flash("Usuario o contraseña invalido")
-            return render_template('index.html')
+        for data in range(len(users)):
+            if user==users[data] and passw==passwords[data]:
+                logged=True
+                return redirect('/Inicio')
+        flash('Usuario o contraseña no coinciden')
+        return redirect('/')
+    return redirect('/')
 
-
-@app.route('/SignUp', methods=['GET','POST'])
+@app.route('/SignUp', methods=['GET'])
 def SignUP():
     return render_template('registro.html')
 
 @app.route('/Sign', methods=['GET','POST'])
 def Sign():
+    if request.method == 'POST':
+        user=request.form['user']
+        passw=request.form['password']
+        users.append(user)
+        passwords.append(passw)
     flash("Ya estás registrado, ¡Inicia sesión!")
     return redirect('/')
 
@@ -59,7 +66,7 @@ def Dash():
         flash("Inicia sesión prueba con 123 y 123, o registrate")
         return redirect('/')
 
-@app.route('/Search', methods=['GET'])
+@app.route('/Search', methods=['GET','POST'])
 def Search():
     if logged:
         return render_template('search.html')
--- static/js/script.js
+++ static/js/script.js
@@ -9,6 +9,7 @@ button_menuBar.onclick = function(){
 let opc = document.querySelectorAll(".op-button");
 let opcs = document.querySelectorAll(".op-menu");
 
+
 for (let i = 0; i < opc.length; i++) {
     opc[i].onclick = function () {
         let j = 0;
@@ -29,17 +30,48 @@ for (let i = 0; i < opc.length; i++) {
 //user profile
 let list = document.querySelectorAll('.item-user');
 let listC = document.querySelectorAll('.content-user')
+let cont = document.querySelector(".contenido");
 for(let i = 0; i < list.length; i++){
     list[i].onclick = function(){
         let j = 0;
         while(j < list.length){
             list[j].className = 'item-user'
             listC[j].className = 'content-user'
+            cont.className='contenido'
             j++;
         }list[i].className = 'item-user select'
         listC[i].className = 'content-user active'
+        if(i==3){
+            cont.className='contenido active'
+        }
     }
 }
+
+let chats = document.querySelectorAll('.user-chat-n');
+let contChats = document.querySelectorAll('.cont-chat-n');
+let backArrow = document.querySelectorAll('#back')
+let contChat = document.querySelector('.contenido-chats')
+
+for(let i = 0; i < chats.length; i++){
+    chats[i].onclick = function(){
+        let j = 0;
+        while(j < chats.length){
+            chats[j].className = 'user-chat-n';
+            contChats[j].className = 'cont-chat-n';
+            j++;
+        }chats[i].className = 'user-chat-n select';
+        contChats[i].className = 'cont-chat-n active';
+        contChat.classList.toggle('on')
+    }
+    backArrow[i].onclick = function(){
+    contChat.classList.toggle('on')
+}
+}
+
+
+
+
+
 //user profile end
 
 //dashboard
