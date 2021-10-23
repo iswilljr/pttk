4278fc584ec47eb9e0b33efbb91bd71ac1db3813
WillJrJU <91134158+WillJrJU@users.noreply.github.com>
2021-10-22 23:06:35 -0500

    Sprint 4 - Implementacion de Base de Datos

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-from flask import Flask, render_template, redirect, request, flash, session
-import os, sqlite3, utils
-from sqlite3 import Error
-from werkzeug.security import generate_password_hash, check_password_hash
-from markupsafe import escape
+from datetime import datetime; from flask import Flask, render_template, redirect, request, flash, session; import os, sqlite3, utils; from sqlite3 import Error; from werkzeug.security import generate_password_hash, check_password_hash; from markupsafe import escape
 
 app = Flask(__name__)
 app.secret_key=os.urandom(24)
 
 @app.route('/', methods=['GET','POST'])
-def Index():
-    if 'user' in session:
-        return redirect('/Inicio')
-    return render_template('index.html')
-
-@app.route('/Login', methods=['GET','POST'])
 def Login():
     if request.method == 'POST':
         user=escape(request.form['user'])
@@ -36,14 +26,10 @@ def Login():
                 return redirect('/')
         except Error:
             print(Error)
-    return redirect('/')
+    return render_template('index.html')
 
-@app.route('/SignUp', methods=['GET','POST'])
+@app.route('/Signup', methods=['GET','POST'])
 def SignUP():
-    return render_template('registro.html')
-
-@app.route('/Sign', methods=['GET','POST'])
-def Sign():
     if request.method == 'POST':
         email=escape(request.form['email'])
         user=escape(request.form['user'])
@@ -51,13 +37,13 @@ def Sign():
         pw_hash=generate_password_hash(pw)
         if not utils.isEmailValid(email):
             flash('Email invalido')
-            return redirect('/SignUp')
+            return redirect('/Signup')
         elif not utils.isUsernameValid(user):
             flash('Usuario invalido')
-            return redirect('/SignUp')
+            return redirect('/Signup')
         elif not utils.isPasswordValid(pw):
             flash('Contraseña invalido')
-            return redirect('/SignUp')
+            return redirect('/Signup')
         try:
             with sqlite3.connect('database.db') as db:
                 cur = db.cursor()
@@ -68,44 +54,238 @@ def Sign():
                     return redirect('/')
                 else:
                     flash('Usuario en uso, pruebe con otro')
-                    return redirect('/SignUp')
+                    return redirect('/Signup')
         except Error:
             print(Error)
-    return redirect('/SignUp')
+    return render_template('registro.html')
 
 @app.route('/Inicio', methods=['GET'])
 def Home():
     if 'user' in session:
-        return render_template('front.html',user=session['user'],rol=session['rol'])
+        try:
+            with sqlite3.connect('database.db') as db:
+                cur = db.cursor()
+                cur.execute("SELECT * FROM Posts ORDER BY id desc")
+                dataPost=cur.fetchall()
+            return render_template('front.html',user=session['user'],rol=session['rol'],    dataPost=dataPost)
+        except Error:
+            print(Error)
     else:
         return redirect('/')
+
+@app.route('/Post',methods=['POST'])
+def Post():
+    if 'user'in session:
+        global message,img,fecha
+        if request.method=='POST':
+            message=str(escape(request.form['text']))
+            file=request.files['fileImg']
+            if file.filename!='':
+                img=f"static/img/{file.filename}"
+                file.save(img)
+            else:
+                img='None'
+            fecha=str(datetime.today()).split(" ")
+            user=session['user']
+            try:
+                with sqlite3.connect('database.db') as db:
+                    cur = db.cursor()
+                    cur.execute("INSERT INTO Posts(username,fecha,mensage,imagen) VALUES(?,?,?,?)",(user,fecha[0],message,img))
+            except Error:
+                print(Error)
+        return redirect('/Inicio')
+    else:
+        return redirect('/')
+
+@app.route('/Post/Edit/<string:user>/<int:id>',methods=['GET','POST'])
+@app.route('/Post/Delete/<string:user>/<int:id>',methods=['GET'])
+def EditDelete(user,id):
+    if 'user' in session:
+        try:
+            with sqlite3.connect('database.db') as db:
+                cur = db.cursor()
+                if request.path==f'/Post/Edit/{user}/{id}' and (session['rol']!='USUARIO' or user==session['user']):
+                    if request.method=='POST':
+                        message=str(escape(request.form['text']))
+                        file=request.files['fileImg']
+                        fecha=str(datetime.today()).split(" ")
+                        if file.filename!='':
+                            img=f"static/img/{file.filename}"
+                            file.save(img)
+                            cur.execute("UPDATE Posts SET mensage=?, imagen=?, fecha=? WHERE id = ?",(message,img,fecha[0],id))
+                        else:
+                            img=cur.execute("SELECT imagen FROM Posts WHERE id = ?",[id]).fetchone()
+                            cur.execute("UPDATE Posts SET mensage=?, imagen=?, fecha=? WHERE id = ?",(message,img[0],fecha[0],id))
+                        return redirect('/Inicio')
+                    data=cur.execute("SELECT * FROM Posts WHERE id = ?",[id]).fetchone()
+                    return render_template('editPost.html',data=data)
+                elif request.path==f'/Post/Delete/{user}/{id}' and (session['rol']!='USUARIO' or user==session['user']):
+                    cur.execute("DELETE FROM Posts WHERE id = ?",[id])
+                    return redirect('/Inicio')
+                else:
+                    return redirect('/Inicio')
+        except Error:
+            print(Error)
+    else:
+        return redirect('/')  
 
 @app.route('/User/<username>', methods=['GET'])
 def Profile(username):
     if 'user' in session:
-        return render_template('user.html',username=username,user=session['user'],rol=session['rol'])
+        try:
+            with sqlite3.connect('database.db') as db:
+                db.row_factory=sqlite3.Row
+                cur = db.cursor()
+                dataUser=cur.execute("SELECT username,email,pais,ciudad,telefono,descripcion FROM Sesiones WHERE username = ?",[username]).fetchone()
+                dataFotos=cur.execute("SELECT imagen FROM Posts WHERE username = ?",[username]).fetchall()
+                if not dataUser:
+                    return f"Usuario {username} no encntrado"
+                return render_template('user.html',username=username,user=session['user'],rol=session['rol'],dataUser=dataUser,dataFotos=dataFotos, dataFotoslen=len(dataFotos))
+        except Error:
+            print(Error)
+    else:
+        return redirect('/')
+
+@app.route('/User/<username>/Edit', methods=['POST'])
+def ProfileEdit(username):
+    if 'user' in session:
+        if request.method=='POST':
+            if username==session['user']:
+                desc=request.form['desc']
+                pais=request.form['pais']
+                ciud=request.form['ciud']
+                tel=request.form['tel']
+                user=request.form['user']
+                email=request.form['email']
+                try:
+                    with sqlite3.connect('database.db') as db:
+                        cur = db.cursor()
+                        userUse=cur.execute("SELECT username FROM Sesiones WHERE username = ?",[user]).fetchone()
+                        if userUse!=None:
+                            print("si")
+                            user=session['user']
+                        if user==session['user']:
+                            cur.execute("UPDATE Sesiones SET email=?,descripcion=?,pais=?,ciudad=?,telefono=? WHERE username=?",(email,desc,pais,ciud,tel,session['user']))
+                        else:
+                            cur.execute("UPDATE Sesiones SET email=?,username=?,descripcion=?,pais=?,ciudad=?,telefono=? WHERE username=?",(email,user,desc,pais,ciud,tel,session['user']))
+                            cur.execute("UPDATE Posts SET username  = ? WHERE username=?",(user,session['user']))
+                            session['user']=user
+                        return redirect(f"/User/{session['user']}")
+                except Error:
+                    print(Error)
+        return redirect('/Inicio')
+    else:
+        return redirect('/')
+
+@app.route('/User/<username>/Delete', methods=['GET'])
+def ProfileDelete(username):
+    if 'user' in session:
+        if session['rol']!='USUARIO':
+            try:
+                with sqlite3.connect('database.db') as db:
+                    cur= db.cursor()
+                    cur.execute("DELETE FROM Posts WHERE username = ?",[username])
+                    cur.execute("DELETE FROM Sesiones WHERE username = ?",[username])
+                    return redirect('/Dash')
+            except Error:
+                print(Error)
+        else:
+            return redirect('/Inicio')
     else:
         return redirect('/')
 
 @app.route('/Dash', methods=['GET'])
 def Dash():
     if 'user' in session:
-        return render_template('dash.html',user=session['user'],rol=session['rol'])
+        try:
+            with sqlite3.connect('database.db') as db:
+                cur = db.cursor()
+                dataPost=cur.execute("SELECT * FROM Posts WHERE username = ? ORDER BY id desc",[session['user']])
+                if dataPost==None:
+                    dataPost='None'
+        except Error:
+            print(Error)
+        return render_template('dash.html',user=session['user'],rol=session['rol'],dataPost=dataPost)
     else:
         return redirect('/')
 
-@app.route('/Search', methods=['GET'])
+@app.route('/Dash/Post', methods=['GET','POST'])
+def DashPost():
+    if 'user' in session:
+        if request.method=='POST':
+            try:
+                id_post=int(escape(request.form['consulta']))
+            except:
+                return redirect('/Dash')
+            try:
+                with sqlite3.connect('database.db') as db:
+                    cur = db.cursor()
+                    dataPost=cur.execute("SELECT * FROM Posts WHERE id = ?",[id_post]).fetchone()
+                    if dataPost==None:
+                        print("si es none")
+                        dataPost='None'
+                    return render_template('dashPost.html',user=session['user'],rol=session['rol'],dataPost=dataPost)
+            except Error:
+                print(Error)
+        return redirect('/Dash')
+    else:
+        return redirect('/')
+
+@app.route('/Dash/User', methods=['GET','POST'])
+def DashUser():
+    if 'user' in session:
+        if request.method=='POST':
+            user=request.form['consulta']
+            if user==session['user']:
+                return redirect('/Dash')
+            try:
+                with sqlite3.connect('database.db') as db:
+                    cur = db.cursor()
+                    dataUser=cur.execute("SELECT username,descripcion,rol FROM Sesiones WHERE username = ?",[user]).fetchone()
+                    dataPost=cur.execute("SELECT * FROM Posts WHERE username = ? ORDER BY id desc",[user]).fetchall()
+                    if not dataPost:
+                        dataPost='None'
+                        print("noo")
+                    if dataUser==None:
+                        dataUser='None'
+                    return render_template('dashUser.html',user=session['user'],rol=session['rol'],dataUser=dataUser,dataPost=dataPost)
+            except Error:
+                print(Error)
+            return redirect('/Dash')
+        return redirect('/Dash')
+    else:
+        return redirect('/')
+
+@app.route('/Dash/User/Rol/<rol>', methods=['POST'])
+def ProfileRol(rol):
+    if 'user' in session:
+        if request.method=='POST':
+            usuario=request.form['user']
+            if session['rol']=='SUPERADMINISTRADOR':
+                if rol=='USUARIOO' or rol=='ADMINISTRADOR' or rol=='SUPERADMINISTRADOR':
+                    try:
+                        with sqlite3.connect('database.db') as db:
+                            cur= db.cursor()
+                            cur.execute("UPDATE Sesiones SET rol = ? WHERE username = ?",(rol,usuario))
+                            return redirect('/Dash')
+                    except Error:
+                        print(Error)
+        return redirect('/Dash')
+    else:
+        return redirect('/')
+
+@app.route('/Search', methods=['GET','POST'])
 def Search():
     if 'user' in session:
         return render_template('search.html',user=session['user'],rol=session['rol'])
     else:
         return redirect('/')
 
-@app.route('/Logout')
+@app.route('/Logout', methods=['GET'])
 def Logout():
     if 'user' in session:
         session.popitem()
-    return redirect('/')
+    return redirect('/')  
 
 if __name__=='__main__':
-    app.run( host='127.0.0.1', port=443, ssl_context=('micertificado.pem', 'llaveprivada.pem'), debug=True)
+    app.run( host='0.0.0.0', port=443, ssl_context=('micertificado.pem', 'llaveprivada.pem'), debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
-let button_menuBar= document.querySelector('.bx-menu')
+let button_menuBar = document.querySelector('.bx-menu')
 let menuBar = document.querySelector('.menu-responsive')
 
-button_menuBar.onclick = function(){
+button_menuBar.onclick = function () {
     menuBar.classList.toggle('active')
 }
 
 //frontpage 
-let opc = document.querySelectorAll(".op-button");
+let opc = document.querySelectorAll(".fa-ellipsis-h");
 let opcs = document.querySelectorAll(".op-menu");
 
 
@@ -23,6 +23,32 @@ for (let i = 0; i < opc.length; i++) {
         } opcs[i].classList.toggle("active")
     }
 }
+
+function file_img() {
+    var archivo = document.getElementById("fileImg").files[0];
+    var reader = new FileReader();
+    if (fileImg) {
+        reader.readAsDataURL(archivo);
+        reader.onloadend = function () {
+            document.getElementById("imgs").className = "img active"
+            document.getElementById("vids").className = "vid"
+            document.getElementById("imgs").src = reader.result;
+        }
+    }
+}
+function file_vid() {
+    var archivo = document.getElementById("fileVid").files[0];
+    var reader = new FileReader();
+    if (fileVid) {
+        reader.readAsDataURL(archivo);
+        reader.onloadend = function () {
+            document.getElementById("vids").className = "vid active"
+            document.getElementById("imgs").className = "img"
+            document.getElementById("vids").src = reader.result;
+        }
+    }
+}
+
 // frontpage end
 
 
@@ -31,19 +57,25 @@ for (let i = 0; i < opc.length; i++) {
 let list = document.querySelectorAll('.item-user');
 let listC = document.querySelectorAll('.content-user')
 let cont = document.querySelector(".contenido");
-for(let i = 0; i < list.length; i++){
-    list[i].onclick = function(){
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
         let j = 0;
-        while(j < list.length){
-            list[j].className = 'item-user'
+        while (j < list.length) {
+            if (list[j].className == 'item-user fotos' || list[j].className == 'item-user fotos select') {
+                list[j].className = 'item-user fotos'
+            } else {
+                list[j].className = 'item-user'
+            }
             listC[j].className = 'content-user'
-            cont.className='contenido'
+            cont.className = 'contenido'
             j++;
-        }list[i].className = 'item-user select'
-        listC[i].className = 'content-user active'
-        if(i==3){
-            cont.className='contenido active'
+        } if (list[i].className == 'item-user fotos') {
+            list[i].className = 'item-user fotos select'
+            cont.className = 'contenido active'
+        } else {
+            list[i].className = 'item-user select'
         }
+        listC[i].className = 'content-user active'
     }
 }
 
@@ -52,24 +84,50 @@ let contChats = document.querySelectorAll('.cont-chat-n');
 let backArrow = document.querySelectorAll('#back')
 let contChat = document.querySelector('.contenido-chats')
 
-for(let i = 0; i < chats.length; i++){
-    chats[i].onclick = function(){
+for (let i = 0; i < chats.length; i++) {
+    chats[i].onclick = function () {
         let j = 0;
-        while(j < chats.length){
+        while (j < chats.length) {
             chats[j].className = 'user-chat-n';
             contChats[j].className = 'cont-chat-n';
             j++;
-        }chats[i].className = 'user-chat-n select';
+        } chats[i].className = 'user-chat-n select';
         contChats[i].className = 'cont-chat-n active';
         contChat.classList.toggle('on')
     }
-    backArrow[i].onclick = function(){
-    contChat.classList.toggle('on')
+    backArrow[i].onclick = function () {
+        contChat.classList.toggle('on')
+    }
 }
+
+let buttonEdit = document.querySelectorAll('.edit-cont')
+let inputEdit = document.querySelectorAll('.input-edit-cont')
+for (let i = 0; i < buttonEdit.length; i++) {
+    let btn = buttonEdit[i];
+    let ipt = inputEdit[i];
+    let btnEdit = document.querySelector('.button-edit');
+    btn.onclick = function () {
+        btnEdit.className = 'button-edit active'
+        let atr = ipt.getAttributeNames()
+        for (let j = 0; j < atr.length; j++) {
+            if (atr[j] == 'disabled') {
+                ipt.removeAttribute('disabled')
+            } else {
+                ipt.setAttribute('disabled', 'disabled')
+            }
+
+        }
+
+    }
 }
 
+function habilitar() {
+    for (let i = 0; i < inputEdit.length; i++) {
+        let ipt = inputEdit[i];
+        ipt.removeAttribute('disabled')
 
-
+    }
+}
 
 
 //user profile end
@@ -77,15 +135,62 @@ for(let i = 0; i < chats.length; i++){
 //dashboard
 let dash_items = document.querySelectorAll(".dash-item");
 let dash_conts = document.querySelectorAll(".dash-content-item")
-for(let i = 0; i<dash_items.length; i++){
-    dash_items[i].onclick = function(){
+let optionDash = document.querySelectorAll('.option-dash')
+for (let i = 0; i < dash_items.length; i++) {
+    dash_items[i].onclick = function () {
+        for (let k = 0; k < optionDash.length; k++) {
+            optionDash[k].removeAttribute('selected');
+        }
         let j = 0;
-        while(j<dash_items.length){
+        while (j < dash_items.length) {
             dash_items[j].className = "dash-item";
             dash_conts[j].className = "dash-content-item";
+
             j++;
         }dash_items[i].className = "dash-item select";
         dash_conts[i].className = "dash-content-item active";
+        if(i==0){
+            optionDash[0].setAttribute('selected', 'selected')
+            formDash.setAttribute('style','display: none;')
+        }else if(i==1){
+            optionDash[1].setAttribute('selected','selected')
+            formDash.setAttribute('style','display: none;')
+        }else if(i==2){
+            optionDash[2].setAttribute('selected','selected')
+        }else if(i==3){
+            optionDash[3].setAttribute('selected','selected')
+        }
     }
+}
+
+function actionSelect(){
+    let selectDash = document.querySelector('.select-dash')
+    let dashConsulta = document.getElementById('dashConsulta')
+    if(selectDash.value=='post' || selectDash.value=='comt'){
+        dashConsulta.action='/Dash/Post'
+    }else if(selectDash.value=='user' || selectDash.value=='asig'){
+        dashConsulta.action='/Dash/User'
+    }
+}
+
+let buttonUserDash = document.querySelectorAll('.button-user-dash')
+let contUserDash = document.querySelectorAll('.opciones-dash')
+for (let i = 0; i < buttonUserDash.length; i++) {
+    let btn = buttonUserDash[i];
+    btn.onclick = function(){
+        contUserDash[i].classList.toggle('active')
+    }
+    
+}
+
+let formRol = document.getElementById('formRol')
+function user(){
+    formRol.action="/Dash/User/Rol/USUARIOO"
+}
+function admin(){
+    formRol.action="/Dash/User/Rol/ADMINISTRADOR"
+}
+function superadmin(){
+    formRol.action="/Dash/User/Rol/SUPERADMINISTRADOR"
 }
 // dashboard end
